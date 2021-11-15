997fd222e141227f57a4c73dbf6e01c4e9d952ce
Do Dinh Nam <namdoel1412@gmail.com>
2021-11-15 19:01:01 +0700

    ll

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from flask import Flask, json, render_template, request, redirect, url_for
 from flask.helpers import make_response
 import pymongo
 from bson.objectid import ObjectId
-from util.http import successResponse
+from util.http import failedResponse, successResponse
 from bson import json_util
 from query.farm_query import lstFarmQueryByOwnerId
 from util.jsonEncoder import JSONEncoder
+from flask import jsonify
+
 
 app = Flask(__name__)
-client = pymongo.MongoClient("mongodb://DODINHNAM:27017")
+client = pymongo.MongoClient("mongodb://DODINHNAM:27017,DODINHNAM:27018,DODINHNAM:27019")
 db = client["pigai"]
 events_collection = db["events"]
 behaviours_collection = db["behaviours"]
@@ -18,24 +20,76 @@ pigs_collection = db["pigs"]
 weighttypes_collection = db["weighttypes"]
 farm_collection = db["farms"]
 
-
-@app.route("/")
-def home():
-	tmp = farm_collection.aggregate(lstFarmQueryByOwnerId("614c2a866a84c95e342dfd4f", 0, 10))
+# FARM - ownerId
+@app.route("/farm/ownerId", methods=["POST"])
+def getFarmByOwnerId():
+	req = request.json
+	_offset = req['offset']
+	_size = req['size']
+	_ownerId = req['ownerId']
+	tmp = farm_collection.aggregate(lstFarmQueryByOwnerId(_ownerId, _offset, _size))
 	c = []
 	for i in tmp:
 		c.append(i)
-	a = list(tmp)
-	print(a)
-	b = None
-	for i in tmp:
-		b = i
-	print(b)
-	#return make_response(successResponse(JSONEncoder().encode(c)))
-	#return make_response(successResponse(json.dumps(c, default=str)))
 	return make_response(successResponse(c))
-	# docs_list  = list(events_collection.find())
-	# return json.dumps(tmp, default=json_util.default)
+
+# FARM - Tạo Farm
+@app.route("/farms", methods=["POST"])
+def home():
+	req = request.json
+	_name = req['name']
+	_address = req['address']
+	_ownerId = req['ownerId']
+	print(_name)
+	print(_address)
+	print(_ownerId)
+	if not _name or not _address or not _ownerId:
+		return make_response(failedResponse('IdsRequired', 'IdsRequired'))
+	data = {
+		"name": _name,
+		"address": _address,
+		"ownerId":  _ownerId
+	}
+	farm_collection.insert_one(data)
+	data['id'] = str(data['_id'])
+	data.pop('_id')
+	print(data)
+	# for i in result:
+	# 	res.append(i)
+	return make_response(successResponse(data))
+
+# FARM - Tạo UpdateFarm
+@app.route("/farms/<string:farmId>", methods=["PUT"])
+def updateFarm(farmId):
+	req = request.json
+	_name = req['name']
+	_address = req['address']
+	_ownerId = req['ownerId']
+	if (not _name or not _ownerId):
+		return failedResponse('BodyRequired', 'BodyRequired')
+	data = {
+		"name": _name,
+		"address": _address,
+		"ownerId":  _ownerId
+	}
+	event = farm_collection.update_one({ "_id": { "$eq": ObjectId(farmId) } },  { "$set": data })
+	# data['id'] = str(data['_id'])
+	# data.pop('_id')
+	# print(data)
+	# for i in result:
+	# 	res.append(i)
+	return make_response(successResponse(data))
+
+@app.route("/farms/<string:farmId>", methods=["DELETE"])
+def deleteFarm(id):
+    if request.method == 'DELETE':
+        if request.json:
+            params = request.json
+        else:
+            params = request.form
+	id = params.get('id')
+	farm_collection.remove({"_id": ObjectId(id)})
+	return make_response(successResponse(True))
 
 
 @app.route("/fnf/create")
